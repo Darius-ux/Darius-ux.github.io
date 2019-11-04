ab97cf6552b2ba5243ae1d75465874b154715a29
Darius-ux <56253245+Darius-ux@users.noreply.github.com>
2019-11-04 10:49:05 +0200

    Add files via upload

--- gyro/gyro.js
+++ gyro/gyro.js
@@ -6,6 +6,6 @@ function on_gyro_data_uab(e)
 {
 	document.getElementById("id_alpha").innerHTML=Math.round(e.alpha * 100)/100;
 	document.getElementById("id_beta").innerHTML=Math.round(e.beta * 100)/100;
-	document.getElementById("id_gamma").innerHTML=math.round(e.gamma * 100)/100;
+	document.getElementById("id_gamma").innerHTML=Math.round(e.gamma * 100)/100;
 }
 
