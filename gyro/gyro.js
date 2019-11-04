af694a7d07d6a89e2555b9286e1fe8e25bc1d676
Darius-ux <56253245+Darius-ux@users.noreply.github.com>
2019-11-04 12:08:02 +0200

    Add files via upload

--- gyro/gyro.js
+++ gyro/gyro.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML="Logic=2019.11.4.2";
+document.getElementById("id_logic_version").innerHTML="Logic=2019.11.4.3";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 window.addEventListener("devicemotion", on_acc_data_uab);
@@ -16,5 +16,12 @@ function on_acc_data_uab (e)
 	document.getElementById("id_acc_x").innerHTML = Math.round(acc.x*100)/100;
 	document.getElementById("id_acc_y").innerHTML = Math.round(acc.y*100)/100;
 	document.getElementById("id_acc_z").innerHTML = Math.round(acc.z*100)/100;
+	
+	var rot_x=Math.atan(acc.x/acc.z)*180;
+	var rot_y=Math.atan(acc.y/acc.z)*180;
+	
+	document.getElementById("id_rot_x").innerHTML =Math.round(rot_x*100)/100;
+	document.getElementById("id_rot_y").innerHTML =Math.round(rot_y*100)/100;
 }
+
 
