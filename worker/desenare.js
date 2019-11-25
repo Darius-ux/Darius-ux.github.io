0462ed56b61a0ca160f1609a725fba0c91396544
Darius-ux <56253245+Darius-ux@users.noreply.github.com>
2019-11-25 12:53:14 +0200

    Add files via upload

--- worker/desenare.js
+++ worker/desenare.js
@@ -1,6 +1,10 @@
-document.getElementById("id_logic").innerHTML="logic=2019.11.25.2";
+document.getElementById("id_logic").innerHTML= "logic=2019.11.25.3";
 document.getElementById("id_start").addEventListener("click", start);
 document.getElementById("id_stop").addEventListener("click", stop);
+
+var timer_id;
+var unghi={};
+unghi.valoare=0;
 
 function desenare(unghi)
 {
@@ -24,13 +28,22 @@ function desenare(unghi)
 }
 
 function start()
-{
-	var unghi={};
-	unghi.valoare=0;
-	setInterval(desenare, 20, unghi);
+{  
+	document.getElementById("id_start").disabled=true;
+	document.getElementById("id_stop").disabled=false;
+		timer_id= setInterval(desenare, 20, unghi);
+	var muncitor = new Worker("prime.js");
+	muncitor.onmessage = function (e)
+	{
+		document.getElementById("id_prime").innerHTML=e.data;
+	}
+
 }
 
 function stop()
 {
-	
+	document.getElementById("id_start").disabled=false;
+	document.getElementById("id_stop").disabled=true;
+
+	clearInterval(timer_id);
 }
